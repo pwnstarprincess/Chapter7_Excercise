ececdd9097bf3f22f812758766f6e318559e093c
Jennifer Pezzulo <jennifer.pezzulo@gmail.com>
2020-03-27 18:17:11 -0600

    create form

--- Chapter7_Excercise/Form1.Designer.vb
+++ Chapter7_Excercise/Form1.Designer.vb
@@ -22,10 +22,75 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.TranslateButton = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.EnglishBox = New System.Windows.Forms.TextBox()
+        Me.TexteseBox = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'TranslateButton
+        '
+        Me.TranslateButton.Location = New System.Drawing.Point(230, 75)
+        Me.TranslateButton.Name = "TranslateButton"
+        Me.TranslateButton.Size = New System.Drawing.Size(241, 64)
+        Me.TranslateButton.TabIndex = 0
+        Me.TranslateButton.Text = "Translate Sentence"
+        Me.TranslateButton.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(21, 25)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(138, 20)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "English Sentence:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(21, 169)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(213, 30)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Textese Sentence:"
+        '
+        'EnglishBox
+        '
+        Me.EnglishBox.Location = New System.Drawing.Point(196, 25)
+        Me.EnglishBox.Name = "EnglishBox"
+        Me.EnglishBox.Size = New System.Drawing.Size(275, 26)
+        Me.EnglishBox.TabIndex = 3
+        '
+        'TexteseBox
+        '
+        Me.TexteseBox.Location = New System.Drawing.Point(196, 169)
+        Me.TexteseBox.Name = "TexteseBox"
+        Me.TexteseBox.ReadOnly = True
+        Me.TexteseBox.Size = New System.Drawing.Size(275, 26)
+        Me.TexteseBox.TabIndex = 4
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(673, 214)
+        Me.Controls.Add(Me.TexteseBox)
+        Me.Controls.Add(Me.EnglishBox)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TranslateButton)
+        Me.Name = "Form1"
+        Me.Text = "Textese Translator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents TranslateButton As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents EnglishBox As TextBox
+    Friend WithEvents TexteseBox As TextBox
 End Class
